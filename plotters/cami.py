cd7704dcb13362f786cfb2c84df8c1dac16a7213
camrdri <142249230+camrdri@users.noreply.github.com>
2024-04-16 15:36:03 -0300

    Create cami.py

---
+++ plotters/cami.py
@@ -0,0 +1,25 @@
+import seaborn.objects as so
+from gapminder import gapminder
+
+
+def plot():
+    figura = (
+        so.Plot(
+            gapminder[gapminder.continent == "America"],
+            x="year",
+            y="lifeExp",
+            color="country",
+        )
+        .add(so.Line())
+        .label(
+            title="Expectativa de vida en America",
+            x="Año",
+            y="Expectativa de vida",
+            color="País",
+        )
+    )
+    return dict(
+        descripcion="Expectativa de vida en países de America a lo largo del tiempo",
+        autor="La cátedra",
+        figura=figura,
+    )
